8bdc989ce82689863e6ef3e9b55175131de06ea9
EC2 Default User <132760017+kyo2929@users.noreply.github.com>
2023-06-09 09:51:36 +0000

    [Update]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to book_path(params.id)
+    redirect_to book_path(book.id)
   end
 
   def show
@@ -16,6 +16,13 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
   
   private
