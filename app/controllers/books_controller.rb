c2bd2614d1ad2df6877442c13f2fcecef32ed56a
EC2 Default User <132760017+kyo2929@users.noreply.github.com>
2023-06-11 06:06:44 +0000

    [Finish]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,7 +27,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      flash[:notice] = "Book was successfull update."
+      flash[:notice] = "Book was successfully update."
       redirect_to book_path(@book.id)
     else
       render :edit
