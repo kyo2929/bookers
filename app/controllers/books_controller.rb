bba1347929546b4321a6a52f1539a7f65709dba8
EC2 Default User <132760017+kyo2929@users.noreply.github.com>
2023-06-11 02:46:47 +0000

    [Update]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,19 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
-   
     @book = Book.new
   end
   
   def create
-    Book.new(book_params)
-    book.save
-    flash[:notice] = "Book was successfully created."
-    redirect_to book_path(book.id)
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
+    
   end
 
   def show
@@ -21,10 +25,14 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "Book was successfull update."
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfull update."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+    
   end
   
   def destroy
